95952bbf6ba9e89976b01f3bae5d2bb55c1f8aa8
Camden Spiewak <spiewac@sunyit.edu>
2015-03-24 15:00:28 -0400

    split dipswitch into two rectangles as designed

--- edison_sdl/edison_dipswitch.c
+++ edison_sdl/edison_dipswitch.c
@@ -3,7 +3,7 @@
 struct edison_dipswitch
 {
 	uint id;
-	struct SDL_Rect rect;
+	struct SDL_Rect rects[2];
 	int status;
 };
 
@@ -14,11 +14,15 @@ edison_dipswitch* edison_create_dipswitch(int pos_x, int pos_y, int width, int h
 
 	//gives the dipswitch a default id of 0
 	dipswitch->id = 0;
-	//creates the dipswitch rectangle
-	dipswitch->rect.x = pos_x;
-	dipswitch->rect.y = pos_y;
-	dipswitch->rect.w = width;
-	dipswitch->rect.h = height;
+	//creates the dipswitch rectangles
+	dipswitch->rect[0].x = pos_x;
+	dipswitch->rect[0].y = pos_y;
+	dipswitch->rect[0].w = width;
+	dipswitch->rect[0].h = height/2;
+	dipswitch->rect[1].x = pos_x;
+	dipswitch->rect[1].y = pos_y + height/2;
+	dipswitch->rect[1].w = width;
+	dipswitch->rect[1].h = height/2;
 	//sets defualt dipswitch state to 0
 	dipswitch->status = 0;
 
